b8452ba70c9cf767e19f30ddd3717f916d77ada7
Vishal2704 <vishalrajendran27@gmail.com>
2024-04-17 10:58:13 +0530

    updated scripts to collide searchresults

--- app.py
+++ app.py
@@ -1,43 +1,42 @@
-from flask import Flask, redirect, render_template, jsonify,request, url_for
+from flask import Flask, redirect, render_template, jsonify, request, url_for
+from werkzeug.urls import url_parse, url_unquote
 
-
-from modules.seggregator import seggregate_input, trim_title, trim_desc
 from modules.components import items
 
 app = Flask(__name__)
 
-#Homepage routes
+# Homepage route
 @app.route('/')
 def index():
     return render_template('index.html')
 
-#About us page routes
+# About us page route
 @app.route("/aboutus")
 def aboutus():
     return render_template('about-us-page.html')
 
-#Saved Project page routes
+# Saved Project page route
 @app.route("/saved")
 def saved():
     return render_template('saved-project.html')
 
-#New projcect page routes
+# New project page routes
 @app.route("/new")
 def new():
-    return render_template('newauto.html',items=items)
+    return render_template('newauto.html', items=items)
+
 @app.route('/newproject', methods=['POST'])
 def newproject():
     global elements
     data = request.json.get('selected_items')
     print(data)
-    elements=seggregate_input(data)
+    elements = seggregate_input(data)
     print(elements)
-    return redirect(url_for('project',title=elements[0], description=elements[1]))
-    
-@app.route("/project")
-def project(title=None, description=None):
-    current = request.url
+    return redirect(url_for('project', title=elements[0], description=elements[1]))
 
+@app.route("/project")
+def project():
+    current = request.url
     print(current)
     return render_template('saved-project1.html', title=elements[0], description=elements[1])
 
